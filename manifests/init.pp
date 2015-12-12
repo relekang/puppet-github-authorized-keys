c4316dd3a428c9b0cfd8f4250740c588b4c30f7b
Rolf Erik Lekang <me@rolflekang.com>
2015-12-12 14:48:43 +0100

    Add support for additional file of authorized keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,6 @@
 define github_authorized_keys (
   $users    = [],
+  $source   = undef,
   $ssh_dir  = "/home/$title/.ssh",
 ) {
   $urls = join($users, ',')
@@ -11,18 +12,43 @@ define github_authorized_keys (
     mode    => '0700',
   }
 
-  ->
-
-  exec { "concat keys for $title":
-    command => "/usr/bin/curl https://github.com/{$urls}.keys > $ssh_dir/authorized_keys",
+  exec { "download github keys for $title":
+    command => "/usr/bin/curl https://github.com/{$urls}.keys > $ssh_dir/authorized_keys_github",
   }
 
-  ->
+  if $source {
+    $concat_requires = [Exec["download github keys for $title"], File["$ssh_dir/authorized_keys_others"]]
+    $key_files = "$ssh_dir/authorized_keys_github $ssh_dir/authorized_keys_others"
+
+    file { "$ssh_dir/authorized_keys_others":
+      ensure  => present,
+      owner   => $title,
+      group   => $title,
+      source  => $source,
+      mode    => '0600',
+    }
+  } else {
+    $concat_requires = [Exec["download github keys for $title"]]
+    $key_files = "$ssh_dir/authorized_keys_github"
+  }
+
+
+
+  exec { "concat keys for $title":
+    command => "/bin/cat $key_files  > $ssh_dir/authorized_keys",
+    require => $concat_requires
+  }
+
+  exec { "remove temp files for $title":
+    command => "/bin/rm $ssh_dir/authorized_keys",
+    require => Exec["concat keys for $title"]
+  }
 
   file { "$ssh_dir/authorized_keys":
     ensure  => present,
     owner   => $title,
     group   => $title,
     mode    => '0600',
+    require => Exec["concat keys for $title"],
   }
 }
